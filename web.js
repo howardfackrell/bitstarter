b6cf320d57996133b066e017cab5a192d588e3ff
Howard Fackrell <hfackrell@gmail.com>
2013-07-10 04:36:02 +0000

    added index.html

--- web.js
+++ web.js
@@ -1,12 +1,22 @@
 var express = require('express');
+var fs = require('fs');
+var Buffere = require('Buffer');
 
 var app = express.createServer(express.logger());
 
+function html(filename) {
+  var html fs.readFileSync(filename, 'utf-8');
+  return html;
+}
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(html('index.html');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+
+
