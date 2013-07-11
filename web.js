d42326e799e0fd2e20bdb7b1f5cb812a06231c32
howard.fackrell <howard.fackrell@octanner.com>
2013-07-10 19:33:35 -0600

    fix bugs

--- web.js
+++ web.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var fs = require('fs');
-var Buffere = require('Buffer');
+//var Buffer = require('Buffer');
 
 var app = express.createServer(express.logger());
 
 function html(filename) {
-  var html fs.readFileSync(filename, 'utf-8');
+  var html = fs.readFileSync(filename, 'utf-8');
   return html;
 }
+
 app.get('/', function(request, response) {
-  response.send(html('index.html');
+  response.send(html('index.html'));
 });
 
 var port = process.env.PORT || 5000;
